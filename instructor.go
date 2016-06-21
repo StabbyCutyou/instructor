e2209d63c638f90872f4535a757add6cb0453abe
StabbyCutyou <smattkelly@gmail.com>
2016-06-21 09:02:04 -0400

    Bump to v0.1.5

--- instructor.go
+++ instructor.go
@@ -29,7 +29,7 @@ type fragment struct {
 type statement []fragment
 
 // Version is the current semver for this tool
-const Version = "0.1.4"
+const Version = "0.1.5"
 
 // Instructor is an instance of the object which will allow you to inspect structs
 type Instructor struct {
